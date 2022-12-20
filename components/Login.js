2806ed3268fc81d6134fc07c6f62282f1d7b2355
Anushtha <anushthaprakash@gmail.com>
2022-12-20 20:56:14 +0530

    changed secrect google

--- components/Login.js
+++ components/Login.js
@@ -16,7 +16,7 @@ function Login() {
         onClick= {signIn}
         className="p-5 bg-blue-500 rounded-full text-white text-center cursor-pointer"
         >
-        Login with Facebook</h1>
+        Login with Google</h1>
     </div>
   )
 }
